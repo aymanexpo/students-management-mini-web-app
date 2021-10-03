1cff860dee59d9dcc62e268cf40ba98690dee73e
Aymane Talibi <aymane.talibi.at@gmail.com>
2021-10-03 20:08:27 +0100

    env setup
    done :
    dependencies added
    and repo created

---
+++ src/main/java/com/talibi/sms/StudentsManagementMiniWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.talibi.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsManagementMiniWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentsManagementMiniWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/talibi/sms/StudentsManagementMiniWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talibi.sms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsManagementMiniWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
